5c7008f19acad59ef83370535516c2224d1703c7
David Macisso <dmacisso@gmail.com>
2019-10-17 14:52:58 -0400

    Finished Authentication Tutorial

--- authentication/server.js
+++ authentication/server.js
@@ -18,8 +18,6 @@ app.post("/users", async (req, res) => {
   try {
     // const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    console.log(salt);
-    console.log(hashedPassword);
     const user = { name: req.body.name, password: hashedPassword };
     users.push(user);
     res.status(201).json({
@@ -31,10 +29,25 @@ app.post("/users", async (req, res) => {
   } catch {
     res.status(500).json({
       status: "failed"
-      
-    })
+    });
   }
 });
+
+app.post("/users/login", async (req, res) => {
+  const user = users.find(user => user.name === req.body.name);
+  if (user === null) {
+    return res.status(400).json({status: "failed",message: "User not found"})
+    }
+    try {
+      if (await bcrypt.compare(req.body.password, user.password)){
+        res.json({status: "Success", message: "User Logged In"})
+      } else {
+        res.json({status: 'Failed', message: "Not Allowed"})
+      }
+    } catch {
+      res.status(500).json({status: "failed"})
+  }
+  });
 
 // Server
 const port = 3000;
