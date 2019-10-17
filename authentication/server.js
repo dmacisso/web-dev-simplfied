29dfa6002d8209ddc396420122f4269b44cda93d
David Macisso <dmacisso@gmail.com>
2019-10-17 12:00:20 -0400

    2nd commit

--- authentication/server.js
+++ authentication/server.js
@@ -1,20 +1,40 @@
 const express = require("express");
 const app = express();
+const bcrypt = require("bcryptjs");
+
+//Body Parser
+app.use(express.json());
 
 // Data
-const users = [
-  
-]
+const users = [];
 
 // ROUTES
 app.get("/users", (req, res) => {
-  res.json(users)
+  res.json(users);
 });
 
 /**Create Users */
-app.post('/users', (req,res) => {
-  
-})
+app.post("/users", async (req, res) => {
+  try {
+    // const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    console.log(salt);
+    console.log(hashedPassword);
+    const user = { name: req.body.name, password: hashedPassword };
+    users.push(user);
+    res.status(201).json({
+      status: "success",
+      data: {
+        user
+      }
+    });
+  } catch {
+    res.status(500).json({
+      status: "failed"
+      
+    })
+  }
+});
 
 // Server
 const port = 3000;
