eed15eef95ab12359dbaf4a3d93aa7761fe58017
dmacisso <dmacisso@gmail.com>
2019-10-17 19:57:47 -0400

    finished fetch api

--- fetchAPI/script.js
+++ fetchAPI/script.js
@@ -1,3 +1,22 @@
-fetch("https://regres.in/api/users")
-  .then(res => console.log(res))
+// fetch("https://regres.in/api/users/23")
+
+
+// fetch('https://jsonplaceholder.typicode.com/todos/5'
+
+fetch("https://regres.in/api/users/23", {
+  // to do more than just get you need to add options
+   method: 'POST',
+   headers: {
+     'Content-Type': 'application/json'
+   },
+   body: JSON.stringify({
+     name: 'User 1',
+
+   })
+
+})
+  .then(res => { 
+  return res.json()
+  })
+  .then(data => console.log(data))
   .catch(err => console.log(`This is the message: ${err}`));
